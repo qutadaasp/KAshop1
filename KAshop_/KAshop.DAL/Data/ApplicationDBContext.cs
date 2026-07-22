e16f874fbda17ab5afc4570c5b15cc9020a55b2d
qutadaasp <qutadaadnan@hotmail.com>
2026-07-22 15:46:09 +0300

    Final Full Crud

---
+++ KAshop_/KAshop.DAL/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using KAshop.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAshop.DAL.Data
+{
+    public class ApplicationDBContext : DbContext
+
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options) { } 
+        
+    }
+}
